2a8f48ce7a955584dc0e9b3a960f76bcf657bec5
nguyenhoangan03 <nguyenhoangan03.work@gmail.com>
2024-04-20 23:05:17 +0700

    requirements analysis, draw use case diagram and erd, set up environment to development

---
+++ AD_Network/src/main/java/com/adcompany/AD_Telecom/AdNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.adcompany.AD_Telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdNetworkApplication.class, args);
+	}
+
+}
---
+++ AD_Network/src/test/java/com/adcompany/AD_Telecom/AdNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adcompany.AD_Telecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
